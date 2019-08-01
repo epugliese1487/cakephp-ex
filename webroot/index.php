50b6a6229223d965fc2cdc2a14a398d402254cd2
epugliese1487 <53570027+epugliese1487@users.noreply.github.com>
2019-08-01 11:58:01 -0300

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Aplicacion de prueba en PHP " . PHP_VERSION . "\n";
 print "Esta es la version modificada de la aplicacion\n";
 ?>
